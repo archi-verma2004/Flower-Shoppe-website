1ee31c4d6964f76ba493c9b8855d4f8e4e9bd9db
ARCHI VERMA <vermaarchi961@gmail.com>
2025-05-09 00:30:24 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,443 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- font awesome link -->
+
+   
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   
+<!-- header start -->
+  <header>
+    <input type="checkbox" name="" id="toggler"></input>
+    <label for="toggler" class="fas fa-bars"></label>
+     <a href="#" class="logo">Flower Shoppe</a>
+    <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#about">about</a>
+        <a href="#products">products</a>
+        <a href="#review">review</a>
+        <a href="#contact">contact</a>
+    </nav>
+
+    <div class="icons">
+        <a href="#" class="fas fa-heart"></a>
+        <a href="#" class="fas fa-shopping-cart"></a>
+        <a href="#" class="fas fa-user"></a>
+    </div>
+ 
+
+  </header>
+<!-- header end -->
+
+<!-- home section begin -->
+
+ <section class="home" id="home">
+  <div class="content">
+    <h3>fresh flowers</h3>
+    <span>natural and beautiful flowers</span>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat modi vero molestiae alias corrupti inventore libero laudantium itaque qui quisquam fugit, molestias ad reiciendis impedit vitae in explicabo quaerat? Aspernatur?</p>
+    <a href="#" class="btn">Shop now</a> 
+</div>
+ </section>
+
+ <!-- home section end -->
+
+
+  <!-- about section starts -->
+
+ <section class="about" id="about">
+
+<h1 class="heading"> <span> about </span> us </h1>
+ <div class="row">
+
+    <div class="video-container">
+        <video src="vedio.mp4" loop autoplay muted></video>
+        <h3>best flowers sellers</h3>
+    </div>
+
+    <div class="content">
+        <h3>why choose us?</h3>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia maxime dolor provident autem? Impedit quisquam similique ipsum dolorem voluptate ipsa laboriosam soluta, quos laudantium ut rem veritatis, repellendus id quaerat!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nisi praesentium ullam, eum, ipsa at quos dicta harum voluptatibus laboriosam veritatis aut magnam perspiciatis sequi error atque vitae quidem laborum.</p>
+    <a href="#" class="btn"> learn more</a>
+    </div>
+
+ </div>
+ </section>
+
+  <!-- about section ends -->
+
+  <!-- icons section starts  -->
+
+  <section class="icons-container">
+
+    <div class="icons">
+        <img src="images/icon-1.png" alt="">
+        <div class="info">
+            <h3>free delivery</h3>
+            <span>on all orders</span>
+        </div>
+    </div>
+
+    <div class="icons">
+        <img src="images/icon-2.png" alt="">
+        <div class="info">
+            <h3>10 days return</h3>
+            <span>money back guarantee</span>
+        </div>
+    </div>
+
+    
+    <div class="icons">
+        <img src="images/icon-3.png" alt="">
+        <div class="info">
+            <h3>offer and gifts</h3>
+            <span>on all orders</span>
+        </div>
+    </div>
+
+    
+    <div class="icons">
+        <img src="images/icon-2.png" alt="">
+        <div class="info">
+            <h3>secure payments</h3>
+            <span>protected by paypal</span>
+        </div>
+    </div>
+
+
+  </section>
+    <!-- icons section ends  -->
+
+    <!-- products section starts -->
+
+    <section class="products" id="products">
+        <h1 class="heading">latest <span>products</span></h1>
+          <div class="box-container">
+
+            <div class="box">
+                <span class="discount">-10%</span>
+                <div class="image">
+                    <img src="prod1.jpg">
+                    <div class="icons">
+                        <a href="#" class="fas fa-heart"></a>
+                        <a href="#" class="cart-btn">add to cart</a>
+                        <a href="#" class="fas fa-share"></a>
+                    
+                    </div>
+                </div>
+                <div class="content">
+                    <h3>flower pot</h3>
+                    <div class="price">200 INR</div>
+
+                </div>
+            </div>
+
+            <div class="box">
+                <span class="discount">-40%</span>
+                <div class="image">
+                    <img src="prod2.jpg">
+                    <div class="icons">
+                        <a href="#" class="fas fa-heart"></a>
+                        <a href="#" class="cart-btn">add to cart</a>
+                        <a href="#" class="fas fa-share"></a>
+                    
+                    </div>
+                </div>
+                <div class="content">
+                    <h3>flower pot</h3>
+                    <div class="price">150 INR</div>
+
+                </div>
+            </div>
+
+            <div class="box">
+                <span class="discount">-90%</span>
+                <div class="image">
+                    <img src="prod3.jpg">
+                    <div class="icons">
+                        <a href="#" class="fas fa-heart"></a>
+                        <a href="#" class="cart-btn">add to cart</a>
+                        <a href="#" class="fas fa-share"></a>
+                    
+                    </div>
+                </div>
+                <div class="content">
+                    <h3>flower pot</h3>
+                    <div class="price">250 INR</div>
+
+                </div>
+            </div>
+
+
+            
+            <div class="box">
+                <span class="discount">-70%</span>
+                <div class="image">
+                    <img src="prod4.jpg">
+                    <div class="icons">
+                        <a href="#" class="fas fa-heart"></a>
+                        <a href="#" class="cart-btn">add to cart</a>
+                        <a href="#" class="fas fa-share"></a>
+                    
+                    </div>
+                </div>
+                <div class="content">
+                    <h3>flower pot</h3>
+                    <div class="price">600 INR</div>
+
+                </div>
+            </div>
+
+            <div class="box">
+                <span class="discount">-60%</span>
+                <div class="image">
+                    <img src="prod5.jpg">
+                    <div class="icons">
+                        <a href="#" class="fas fa-heart"></a>
+                        <a href="#" class="cart-btn">add to cart</a>
+                        <a href="#" class="fas fa-share"></a>
+                    
+                    </div>
+                </div>
+                <div class="content">
+                    <h3>flower pot</h3>
+                    <div class="price">700 INR</div>
+
+                </div>
+            </div>
+
+            <div class="box">
+                <span class="discount">-50%</span>
+                <div class="image">
+                    <img src="prod6.jpg">
+                    <div class="icons">
+                        <a href="#" class="fas fa-heart"></a>
+                        <a href="#" class="cart-btn">add to cart</a>
+                        <a href="#" class="fas fa-share"></a>
+                    
+                    </div>
+                </div>
+                <div class="content">
+                    <h3>flower pot</h3>
+                    <div class="price">300 INR</div>
+
+                </div>
+            </div>
+
+            
+            <div class="box">
+                <span class="discount">-22%</span>
+                <div class="image">
+                    <img src="prod7.jpg">
+                    <div class="icons">
+                        <a href="#" class="fas fa-heart"></a>
+                        <a href="#" class="cart-btn">add to cart</a>
+                        <a href="#" class="fas fa-share"></a>
+                    
+                    </div>
+                </div>
+                <div class="content">
+                    <h3>flower pot</h3>
+                    <div class="price">764 INR</div>
+
+                </div>
+            </div>
+
+            <div class="box">
+                <span class="discount">-20%</span>
+                <div class="image">
+                    <img src="prod8.jpg">
+                    <div class="icons">
+                        <a href="#" class="fas fa-heart"></a>
+                        <a href="#" class="cart-btn">add to cart</a>
+                        <a href="#" class="fas fa-share"></a>
+                    
+                    </div>
+                </div>
+                <div class="content">
+                    <h3>flower pot</h3>
+                    <div class="price">400 INR</div>
+
+                </div>
+            </div>
+
+
+
+          </div>
+    
+    </section>
+
+        <!-- products section ends -->
+
+        <!-- review section start -->
+
+        <section class="review" id="review">
+            <h1 class="heading">customer's <span>review</span></h1>
+            <div class="box-container">
+                <div class="box">
+                    <div class="stars">
+                     <i class="fas fa-star"></i>
+                     <i class="fas fa-star"></i>
+                     <i class="fas fa-star"></i>
+                     <i class="fas fa-star"></i>
+                     <i class="fas fa-star"></i>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem sapiente distinctio a accusamus exercitationem, harum et quibusdam reiciendis atque deserunt vero quas odit vitae dolorem, est quaerat molestias fugiat ullam?</p>
+                <div class="users">
+                    <img src="pic5.jpg">
+                    <div class="user-info">
+                        <h3>Ananya Pandey</h3>
+                        <span>happy customer</span>
+                    </div>
+                </div>
+                <span class="fas fa-quote-right">
+
+                </span>
+                </div>
+
+                <div class="box">
+                    <div class="stars">
+                     <i class="fas fa-star"></i>
+                     <i class="fas fa-star"></i>
+                     <i class="fas fa-star"></i>
+                     <i class="fas fa-star"></i>
+                     <i class="fas fa-star"></i>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem sapiente distinctio a accusamus exercitationem, harum et quibusdam reiciendis atque deserunt vero quas odit vitae dolorem, est quaerat molestias fugiat ullam?</p>
+                <div class="users">
+                    <img src="pic2.jpg">
+                    <div class="user-info">
+                        <h3>Hritik Roshan</h3>
+                        <span>happy customer</span>
+                    </div>
+                </div>
+                <span class="fas fa-quote-right">
+
+                </span>
+                </div>
+
+                <div class="box">
+                    <div class="stars">
+                     <i class="fas fa-star"></i>
+                     <i class="fas fa-star"></i>
+                     <i class="fas fa-star"></i>
+                     <i class="fas fa-star"></i>
+                     <i class="fas fa-star"></i>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem sapiente distinctio a accusamus exercitationem, harum et quibusdam reiciendis atque deserunt vero quas odit vitae dolorem, est quaerat molestias fugiat ullam?</p>
+                <div class="users">
+                    <img src="pic3.jpg">
+                    <div class="user-info">
+                        <h3>shradhha kapoor</h3>
+                        <span>happy customer</span>
+                    </div>
+                </div>
+                <span class="fas fa-quote-right">
+
+                </span>
+                </div>
+
+
+            </div>
+        </section>
+
+
+           <!-- review section end -->
+
+ <!-- contact section STARTS -->
+           <section class="contact" id="contact">
+            <h1 class="heading">
+                <span>Contact</span> Us
+            </h1>
+
+            <div class="row">
+                         
+              
+                    <form method="POST">
+                        <input type="text" placeholder="Enter Name" class="box" name="name">
+                        <input type="email" placeholder="Enter email" class="box" name="email">
+                        <input type="number" placeholder="Mobile No" class="box" name="phone">
+                        <textarea name="message" class="box" placeholder="message" id="" cols="30" rows="10">      </textarea>
+                            <input type="submit" value="send message"  name="submit" class="btn">
+                   
+                    </form>  
+                    
+                    <?php 
+                          $con=mysqli_connect('localhost','root','rakhi12@','data');
+
+                          if(isset($_POST['submit']))
+                          {
+                             $name=$_POST['name'];
+                             $email=$_POST['email'];
+                             $phone=$_POST['phone'];
+                             $message=$_POST['message'];
+                              
+                              $query="INSERT INTO info(name,email,phone,message)values('$name','$email','$phone','$message')";
+           
+                              $execute=mysqli_query($con,$query);
+                             
+                          }
+           
+           
+                    ?>
+           
+            </div>
+           </section>
+    <!-- contact section ends  -->
+
+           <!-- footer section starts -->
+
+           <section class="footer">
+               <div class="box-container">
+                <div class="box">
+                    <h3>quick links</h3>
+                    <a href="#">home</a>
+                    <a href="#">about</a>
+                    <a href="#">products</a>
+                    <a href="#">review</a>
+                    <a href="#">contact</a>
+                </div>
+
+                <div class="box">
+                    <h3>extra links</h3>
+                    <a href="#">my account</a>
+                    <a href="#">my order</a>
+                    <a href="#">my favourite</a>
+                   
+                </div>
+
+                <div class="box">
+                    <h3>locations</h3>
+                    <a href="#">India</a>
+                    <a href="#">USA</a>
+                    <a href="#">France</a>
+                    <a href="#">Rome</a>
+                   
+                </div>
+
+                <div class="box">
+                    <h3>contact us</h3>
+                    <a href="#">+123-456-7890</a>
+                    <a href="#">examplegamil.com</a>
+                    <a href="#">Kolkata, india - 708987</a>
+                   
+                </div>
+
+               </div>
+
+               <div class="credit">
+                 created by <span>Ms Archi Verma</span> & <span>Ms. Apurva Rani</span> | all rights reserved </div>
+               </div>
+           </section>
+           <!-- footer section ends -->
+
+</body>
+</html>
